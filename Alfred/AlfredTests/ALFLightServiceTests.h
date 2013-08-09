515e52ae2c39db47d06de3bed0a1b7ed4c1acc6e
Steven Boles <boles.steven@gmail.com>
2013-08-09 14:18:12 -0400

    implemented updateStatusForProject
    mocked out web service calls in test
    added updateStatusForProject
    fixed bug in updateStatusForLight
    
    updateStatusForProject
    - make url from url + /api/json
    - issue request
    - interpret results
    - get latestBuild url
    - make a second http request for latestBuild url + /api/json
    - interpret results
    - update project status

--- Alfred/AlfredTests/ALFLightServiceTests.h
+++ Alfred/AlfredTests/ALFLightServiceTests.h
@@ -6,9 +6,10 @@
 //  Copyright (c) 2013 Rally Software. All rights reserved.
 //
 
+#import <objc/runtime.h>
 #import "ALFAbstractDataTests.h"
 #import "ALFLightService.h"
 
-@interface ALFLightServiceTests : ALFAbstractDataTests
+@interface ALFLightServiceTests : ALFAbstractDataTests 
 
 @end
--- Alfred/AlfredTests/ALFLightServiceTests.m
+++ Alfred/AlfredTests/ALFLightServiceTests.m
@@ -10,6 +10,20 @@
 
 @implementation ALFLightServiceTests
 
+- (void) setUp {
+    [super setUp];
+
+    method_exchangeImplementations(class_getInstanceMethod([ALFLightService class], @selector(getProjectDataWith:)), class_getInstanceMethod([self class], @selector(mockGetProjectDataWith:)));
+    method_exchangeImplementations(class_getInstanceMethod([ALFLightService class], @selector(getBuildDataWith:)), class_getInstanceMethod([self class], @selector(mockGetBuildDataWith:)));
+}
+
+- (void) tearDown {
+    method_exchangeImplementations(class_getInstanceMethod([self class], @selector(mockGetProjectDataWith:)), class_getInstanceMethod([ALFLightService class], @selector(getProjectDataWith:)));
+    method_exchangeImplementations(class_getInstanceMethod([self class], @selector(mockGetBuildDataWith:)), class_getInstanceMethod([ALFLightService class], @selector(getBuildDataWith:)));
+    
+    [super tearDown];
+}
+
 - (void)testAllLights
 {
     // setup
@@ -57,24 +71,103 @@
     return nil;
 }
 
-- (void)testUpdateOverallStatusForLight {
+- (void)testUpdateOverallStatusForGreenLightToRedLight {
     // setup
+    method_exchangeImplementations(class_getInstanceMethod([ALFLightService class], @selector(updateStatusForProject:)), class_getInstanceMethod([self class], @selector(mockUpdateStatusForProject:)));
     NSManagedObject* project = [self makeProject];
     NSManagedObject* light = [project valueForKey:@"light"];
     [project setValue:@"FAILED" forKey:@"status"];
+    STAssertEquals([[light valueForKey:@"overallStatus"] boolValue], YES, @"status should be YES");
     
     // test
     ALFLightService *service = [[ALFLightService alloc] initWithManagedObjectContext: self.moc];
     [service updateOverallStatusForLight: light];
-    STAssertEquals([[light valueForKey:@"overallStatus"] boolValue], NO, @"status should be YES");
+    STAssertEquals([[light valueForKey:@"overallStatus"] boolValue], NO, @"status should be NO");
+    
+    // teardown
+    method_exchangeImplementations(class_getInstanceMethod([self class], @selector(mockUpdateStatusForProject:)), class_getInstanceMethod([ALFLightService class], @selector(updateStatusForProject:)));
+}
+
+- (void)testUpdateOverallStatusForRedLightToGreenLight {
+    // setup
+    method_exchangeImplementations(class_getInstanceMethod([ALFLightService class], @selector(updateStatusForProject:)), class_getInstanceMethod([self class], @selector(mockUpdateStatusForProject:)));
+    NSManagedObject* project = [self makeProject];
+    NSManagedObject* light = [project valueForKey:@"light"];
+    [light setValue:NO forKey:@"overallStatus"];
+    [project setValue:@"SUCCESS" forKey:@"status"];
+    STAssertEquals([[light valueForKey:@"overallStatus"] boolValue], NO, @"status should be NO");
+    
+    // test
+    ALFLightService *service = [[ALFLightService alloc] initWithManagedObjectContext: self.moc];
+    [service updateOverallStatusForLight: light];
+    STAssertEquals([[light valueForKey:@"overallStatus"] boolValue], YES, @"status should be YES");
+    
+    // teardown
+    method_exchangeImplementations(class_getInstanceMethod([self class], @selector(mockUpdateStatusForProject:)), class_getInstanceMethod([ALFLightService class], @selector(updateStatusForProject:)));
 }
 
 - (void) testUpdateStatusForProject {
     // setup
     NSManagedObject* project = [self makeProject];
     ALFLightService *service = [[ALFLightService alloc] initWithManagedObjectContext: self.moc];
+    
+    // test
     [service updateStatusForProject: project];
     STAssertEqualObjects([project valueForKey:@"status"], @"SUCCESS", @"status should be SUCCESS");
+}
+
+- (NSManagedObject*) mockUpdateStatusForProject:(NSManagedObject*)project {
+    return project;
+}
+
+- (NSString *) mockGetProjectDataWith:(NSString *)url{
+    return [ALFLightServiceTests greenMockData];
+}
+
+- (NSString *) mockGetBuildDataWith:(NSString *)url{
+    return [ALFLightServiceTests greenBuildData];
+}
+
++ (NSString *) greenMockData {
+    return @""
+    "{"
+    "  \"lastBuild\": {"
+    "    \"number\": 813,"
+    "    \"url\": \"http://alm-build:8080/hudson/job/master-alm-continuous/813/\""
+    "  },"
+    "  \"lastSuccessfulBuild\": {"
+    "    \"number\": 813,"
+    "    \"url\": \"http://alm-build:8080/hudson/job/master-alm-continuous/813/\""
+    "  }"
+    "}";
+}
+
++ (NSString *) redMockData {
+    return @""
+    "{"
+    "  \"lastBuild\": {"
+    "    \"number\": 808,"
+    "    \"url\": \"http://alm-build:8080/hudson/job/master-alm-continuous/808/\""
+    "  },"
+    "  \"lastSuccessfulBuild\": {"
+    "    \"number\": 813,"
+    "    \"url\": \"http://alm-build:8080/hudson/job/master-alm-continuous/813/\""
+    "  }"
+    "}";
+}
+
++ (NSString *) greenBuildData {
+    return @""
+    "{"
+    "  \"result\": \"SUCCESS\""
+    "}";
+}
+
++ (NSString *) redBuildData {
+    return @""
+    "{"
+    "  \"result\": \"FAILURE\""
+    "}";
 }
 
 @end
